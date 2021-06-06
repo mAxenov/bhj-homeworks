a2d6019fa1df44d1e704c5cf69a35d8b0140b408
Maxim <maxime.axenoff@yandex.ru>
2021-06-06 20:41:43 +0500

    element-search/menu/task.js

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,10 +1,15 @@
 "use strict";
 
 const menu = document.querySelectorAll(".menu__link");
+console.log(menu);
 
 for (let i = 0; i < menu.length; i++) {
     menu[i].onclick = () => {
         const menuItem = document.querySelectorAll(".menu_sub");
+        if (menu[i].closest("li").querySelector(".menu_sub").classList.contains("menu_active")) {
+            menu[i].closest("li").querySelector(".menu_sub").classList.remove("menu_active");
+            return false;
+        }
         for (let i = 0; i < menuItem.length; i++) {
             menuItem[i].classList.remove("menu_active");
         }
